6d647f3bbee27608335cfe81a252b6b9e4ef3b6d
Oscar Borgström <oscbor@student.chalmers.se>
2018-08-03 16:44:20 +0100

    2.0 Compliant
    Tools to parse data and socket to send data to Unreal. Unreal project to recieve the data. Requires Unreal engine 4.13 to run. Old project saved in zip under Eel/4.13.

---
+++ Unreal_Adapter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Unreal_Adapter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Buro Happold")]
+[assembly: AssemblyProduct("Unreal_Adapter")]
+[assembly: AssemblyCopyright("Copyright © Buro Happold 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e439a03a-0265-48ff-b6b2-aa41a14632d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unreal_Engine/Create/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.VirtualReality;
+using BH.oM.CFD.Elements;
+using BH.oM.Geometry;
+
+namespace BH.Engine.VirtualReality.Unreal
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Project Project( string name, int saveindex, int scale, string unit = "", List<Streamer> streamers = null, List<UnrealGeometry> geometry = null)
+        {
+            return new Project
+            {
+                Geometry = geometry,
+                Streamers = streamers,
+                saveIndex = saveindex.ToString(),
+                Name = name,
+                Scale = scale.ToString(),
+                Unit = unit,
+            };
+        }
+
+        /***************************************************/
+    }
+}
