b60642eb5811fc6af6ee36e8a5705cb2a9b35c88
Erik Narhi <Erik.Narhi@burohappold.com>
2018-11-28 16:00:46 -0800

    V4.16 Forward Changes In Target Handling
    TargetInfo to ReadOnlyTargetRules and header include order tweaks, code now compiling in 4.16 on

--- Unreal_Plugin/Unreal_Toolkit/Source/Unreal_Toolkit/BHoMSocket.cpp
+++ Unreal_Plugin/Unreal_Toolkit/Source/Unreal_Toolkit/BHoMSocket.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BHoMSocket.h"
 #include "Unreal_Toolkit.h"
+#include "BHoMSocket.h"
 
 
 
--- Unreal_Plugin/Unreal_Toolkit/Source/Unreal_ToolkitEditor.Target.cs
+++ Unreal_Plugin/Unreal_Toolkit/Source/Unreal_ToolkitEditor.Target.cs
@@ -5,21 +5,15 @@ using System.Collections.Generic;
 
 public class Unreal_ToolkitEditorTarget : TargetRules
 {
-	public Unreal_ToolkitEditorTarget(TargetInfo Target)
+	public Unreal_ToolkitEditorTarget(TargetInfo Target) : base (Target)
 	{
 		Type = TargetType.Editor;
+
+        ExtraModuleNames.Add("Unreal_Toolkit");
 	}
 
 	//
 	// TargetRules interface.
 	//
 
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "Unreal_Toolkit" } );
-	}
 }
