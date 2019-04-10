13c3c31fd7e3b74edfe111b991548d7a87685b37
Erik Narhi <Erik.Narhi@burohappold.com>
2019-04-10 12:23:47 -0700

    Folder structure and solution generation fixed
    Solution now generates properly in VS, and the folder structure for the plugin source is reorganized accordingly.

---
+++ Unreal_Plugin/UnrealToolkitProject/Source/UnrealToolkitProject/UnrealToolkitProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealToolkitProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealToolkitProject, "UnrealToolkitProject" );
