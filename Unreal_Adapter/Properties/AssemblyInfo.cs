67f461c66711fc5c4e7d50147ffa2dd23d9728b9
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:04 +0100

    Update Assembly Information

--- Unreal_Adapter/Properties/AssemblyInfo.cs
+++ Unreal_Adapter/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Unreal_Adapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Unreal_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Buro Happold")]
 [assembly: AssemblyProduct("Unreal_Adapter")]
@@ -54,8 +54,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
 
 
 
