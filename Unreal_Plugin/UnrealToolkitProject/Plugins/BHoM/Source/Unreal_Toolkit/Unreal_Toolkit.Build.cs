4b825a38f5467fdf7353ea4675b262013be8cf2e
BHoMBot <BHoMBot@burohappold.com>
2024-01-02 14:20:57 +0000

    Uptick copyright headers to 2024

--- Unreal_Plugin/UnrealToolkitProject/Plugins/BHoM/Source/Unreal_Toolkit/Unreal_Toolkit.Build.cs
+++ Unreal_Plugin/UnrealToolkitProject/Plugins/BHoM/Source/Unreal_Toolkit/Unreal_Toolkit.Build.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -43,6 +43,7 @@ public class Unreal_Toolkit : ModuleRules
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 }
+
 
 
 
